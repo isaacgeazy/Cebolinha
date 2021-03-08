85b6073452fa66e1f52519df8cffc9f8e17ae920
Isaac <isaacgeazy@gmail.com>
2021-03-08 13:32:34 -0300

    Atividade Cebolinha

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string diga;
+
+           Console.ForegroundColor = ConsoleColor.Green;
+           Console.WriteLine("-------------- Fale igual ao Cebolinha ---------------");
+           Console.ResetColor();
+           Console.Write("Digite algo: ");
+           diga = Convert.ToString(Console.ReadLine());
+           Console.WriteLine();
+           Console.ForegroundColor = ConsoleColor.Green;
+           Console.WriteLine($"{diga.Replace("r", "l")}");
+           Console.ResetColor();
+             
+             
+        }
+    }
+}
